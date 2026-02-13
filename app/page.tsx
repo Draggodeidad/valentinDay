2da1ec32fec26703a87a78b4ed2865f74934bdde
Draggodeidad <antonioimanol7@gmail.com>
2026-02-13 17:09:32 -0600

    corazon

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import ValentineScene from "./components/ValentineScene";
 import PhotoGallery from "./components/PhotoGallery";
+import HeartDraw from "./components/HeartDraw";
 
 export default function Home() {
   return (
     <main>
       <ValentineScene />
       <PhotoGallery />
+      <HeartDraw />
     </main>
   );
 }
