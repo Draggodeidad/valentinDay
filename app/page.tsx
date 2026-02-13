ed4236203c6556737f6112d4021a058e596773a6
Draggodeidad <antonioimanol7@gmail.com>
2026-02-13 16:59:24 -0600

    parte de nuestra historia

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
 import ValentineScene from "./components/ValentineScene";
+import PhotoGallery from "./components/PhotoGallery";
 
 export default function Home() {
-  return <ValentineScene />;
+  return (
+    <main>
+      <ValentineScene />
+      <PhotoGallery />
+    </main>
+  );
 }
